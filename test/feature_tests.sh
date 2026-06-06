8c1d749fea10edc9af3d9f518e2960523ce2516d
Paolo Cermelli <paolo.cermelli@mail.polimi.it>
2026-06-06 16:59:44 +0000

    minor changes at feturetest

--- test/feature_tests.sh
+++ test/feature_tests.sh
@@ -15,17 +15,14 @@ echo "Hardware info saved to test/hw.info"
 
 echo ""
 echo "--------- 1. SCHWARZ TEST ---------"
-echo "Varying grid size (n)"
    OMP_NUM_THREADS=2 mpirun -np 4 ./solver --n 256 --mode HYBRID --bc_type ROBIN --case POLYNOMIAL --algo SCHWARZ --inner_it 20 --vtk_type SURFACE | tee -a test/data/serial_runs.log
 
 echo ""
 echo "--------- 2. NEUMANN JACOBI TEST ---------"
-echo "Varying grid size (n)"
    OMP_NUM_THREADS=2 mpirun -np 4 ./solver --n 256 --mode HYBRID --bc_type NEUMANN --case SINUSOIDAL --algo POINT_JACOBI --inner_it 20 --vtk_type SURFACE | tee -a test/data/serial_runs.log
 
 echo ""
 echo "--------- 3. CUSTOM  TEST ---------"
-echo "Varying grid size (n)"
    OMP_NUM_THREADS=2 mpirun -np 4 ./solver --n 128 --mode HYBRID --bc_type DIRICHLET --eq_f "8 * pi^2 * sin(2 * pi * x) * sin(2 * pi * y)" --eq_g "0.0" --eq_uex "sin(2 * pi * x) * sin(2 * pi * y)" | tee -a test/data/serial_runs.log
 
 echo ""
